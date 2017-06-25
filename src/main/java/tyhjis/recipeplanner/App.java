832034ef4757becccdc82b3bb09a5d716ed3bcdb
Kristian <kristian.heikinpoika@gmail.com>
2017-06-25 17:03:41 +0300

    Changed name.

--- src/main/java/tyhjis/recipeplanner/App.java
+++ src/main/java/tyhjis/recipeplanner/App.java
@@ -1,8 +1,6 @@
 package tyhjis.recipeplanner;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -16,7 +14,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/tyhjis/recipeplanner/app.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/tyhjis/recipeplanner/categories.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setTitle("Recipe planner");
         primaryStage.setScene(scene);
